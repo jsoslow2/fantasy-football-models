22b33f7e4a4b3430d1c3f7270f085f1b298e14da
Jack Soslow <jsoslow2@gmail.com>
2023-08-20 18:27:05 -0700

    basic html setup

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,15 @@
-from flask import Flask
-
-app = Flask(__name__)
+from app import app
+from flask import render_template
 
 
 @app.route('/')
-def index():
-    return "Hello, World!"
+@app.route('/draft')
+def draft():
+    # Mock data for demonstration. Replace this with your actual data.
+    players = [
+        {"name": "Player 1", "position": "RB", "predicted_points": 120},
+        {"name": "Player 2", "position": "WR", "predicted_points": 100},
+        # ... add more players as needed
+    ]
+
+    return render_template('draft_template.html', players=players)
