7da8806bb01321e6d248b7a872bcbbb83963d649
Jack Soslow <jsoslow2@gmail.com>
2023-09-04 13:47:58 -0700

    adding init.py

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+import os
+
+# Import routes at the bottom to avoid circular imports
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'fallback_key')
+
+from app import routes
